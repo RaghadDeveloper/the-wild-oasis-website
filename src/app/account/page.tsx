e30889593d6de596fc8f343cf0c46ab471112d35
RaghadDeveloper <raghadmh620@gmail.com>
2026-09-09 07:19:01 +0300

    feat: setup project structures

---
+++ src/app/account/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guest area",
+};
+
+const Account = () => {
+  return <div>Account</div>;
+};
+
+export default Account;
---
+++ src/app/cabins/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cabins",
+};
+
+const Cabins = () => {
+  return <div>Cabins</div>;
+};
+
+export default Cabins;
